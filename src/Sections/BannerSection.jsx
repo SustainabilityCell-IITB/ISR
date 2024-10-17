465e4140f843c46d4dec91602275008622ea2af4
SustainabilityCell-IITB <sustainabilitycelliitb@gmail.com>
2024-10-18 01:03:22 +0530

    Banner Section

--- src/Sections/BannerSection.jsx
+++ src/Sections/BannerSection.jsx
@@ -1,9 +1,14 @@
 import "./BannerSection.css"
+import IITBLogo from "../assets/images/IITB-Logo.png"
 
 function BannerSection(){
     return(
-        <div className="Section" id="Home">
-            <div className="BannerHeading">Heading</div>
+        <div className="Section BannerSection" id="Home">
+            <div className="BannerHeading">
+                <img src={IITBLogo  } alt="" />
+                <h1>IIT BOMBAY</h1>
+                <h2>SUSTAINABLE <br /> DEVELOPMENT <br /> GOALS <br /> REPORT <br /> <span>2023</span></h2>
+            </div>
         </div>
     )
 }
